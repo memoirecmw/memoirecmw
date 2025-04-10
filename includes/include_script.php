eb309dbaab75c6ab44107d5c2a2ef156436ed5e4
Prénom NOM <159848543+analysecinemaxmt@users.noreply.github.com>
2025-04-10 20:43:51 +0200

    Script full calendar

--- includes/include_script.php
+++ includes/include_script.php
@@ -3,3 +3,4 @@
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/quill@2.0.0-beta.0/dist/quill.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/fullcalendar-scheduler@6.1.17/index.global.min.js"></script>
